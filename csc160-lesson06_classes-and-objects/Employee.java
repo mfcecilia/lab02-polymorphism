0c6009f69ef0669243efce819b4f503614fe2dc0
Evelyn Garcia <eyrv09@gmail.com>
2017-10-01 23:51:43 -0600

    correct compiled lesson 6

--- csc160-lesson06_classes-and-objects/Employee.java
+++ csc160-lesson06_classes-and-objects/Employee.java
@@ -31,117 +31,120 @@ import static java.lang.System.out;
  */
 public class Employee
 {
-	/**
-	 * The employee's first name (assuming Western World naming).
-	 */
-	private String firstNameField;
+/**
+ * The employee's first name (assuming Western World naming).
+ */
+private String firstNameField;
 
-	/**
-	 * The employee's last name (assuming Western World naming).
-	 */
-	private String lastNameField;
+/**
+ * The employee's last name (assuming Western World naming).
+ */
+private String lastNameField;
 
-	/**
-	 * Employee's salary (assuming USA dollars).
-	 */
-	private double salaryField;
+/**
+ * Employee's salary (assuming USA dollars).
+ */
+private double salaryField;
 
-	/**
-	 * Constructor.
-	 *
-	 * @param firstName     employee's first name (assuming Western World naming)
-	 * @param lastName      employee's last name (assuming Western World naming)
-	 * @param initialSalary	employee's initial salary (assuming USA dollars)
-	 */
-	public Employee(String firstName, String lastName, double initialSalary)
-	{
-		firstNameField = Employee.firstName;
-		lastNameField = Employee.lastName;
-		salaryField = Employee.initialSalary;
+/**
+ * Constructor.
+ *
+ * @param firstName     employee's first name (assuming Western World naming)
+ * @param lastName      employee's last name (assuming Western World naming)
+ * @param initialSalary	employee's initial salary (assuming USA dollars)
+ */
+public Employee(String firstName, String lastName, double initialSalary)
+{
+	firstNameField = firstName;
+	lastNameField = lastName;
+	salaryField = initialSalary;
+	//out.println("Constructor implemented");
+}
 
-		out.println("Constructor has been implemented");
-	}
+Employee person = new Employee("firstName", "lastName", 25);
 
-	/**
-	 * Print on the standard output the details of this employee.
-	 */
-	public void displayValues()
-	{
-		out.println("First Name: " + firstName + " Last Name: " + lastName + " Initial Salary: " + initialSalary);
-	}
+/**
+ * Print on the standard output the details of this employee.
+ */
+public void displayValues () {
 
-	/**
-	 * Getter for the employee's first name (assuming Western World naming).
-	 *
-	 * @return	employee's first name.
-	 */
-	public String getFirst()
-	{
-		return Employee.firstNameField;
-	}
+	out.println("First Name: " + person.firstNameField + " Last Name: " + person.lastNameField + " Salary: " + person.salaryField);
+}
 
-	/**
-	 * Setter for the employee's first name (assuming Western World naming).
-	 *
-	 * @param firstName	employee's first name (assuming Western World naming).
-	 */
-	public void setFirst(String firstName)
-	{
-		Employee.firstNameField = firstName;
-		out.println("First Name: " + firstName);
-	}
+/**
+ * Getter for the employee's first name (assuming Western World naming).
+ *
+ * @return	employee's first name.
+ */
+public String getFirst()
+{
+		return person.firstNameField;
+}
 
-	/**
-	 * Getter for the employee's last name (assuming Western World naming).
-	 *
-	 * @return	employee's last name.
-	 */
-	public String getLast()
-	{
-		return Employee.lastNameField;
-	}
+/**
+ * Setter for the employee's first name (assuming Western World naming).
+ *
+ * @param firstName	employee's first name (assuming Western World naming).
+ */
+public void setFirst(String firstName)
+{
+	person.firstNameField = firstName;
+	out.println("First Name: " + getFirst());
+}
 
-	/**
-	 * Setter for the employee's last name (assuming Western World naming).
-	 *
-	 * @param lastName	employee's last name (assuming Western World naming).
-	 */
-	public void setLast(String lastName)
-	{
-		Employee.lastNameField = lastName;
-		out.println("Last Name: " + lastName);
-	}
+/**
+ * Getter for the employee's last name (assuming Western World naming).
+ *
+ * @return	employee's last name.
+ */
+public String getLast()
+{
+	return person.lastNameField;
+}
 
-	/**
-	 * Getter for the monthly salary.
-	 *
-	 * @return	the value of the monthly salary
-	 */
-	public double getMonthlySalary()
-	{
-		out.println("Get Monthly Salary: " + Employee.salaryField/12);
-		return Employee.salaryField/12;
-	}
+/**
+ * Setter for the employee's last name (assuming Western World naming).
+ *
+ * @param lastName	employee's last name (assuming Western World naming).
+ */
+public void setLast(String lastName)
+{
+	person.lastNameField = lastName;
+	out.println("Last Name: " + getLast());
+}
 
-	/**
-	 * Setter for the monthly salary.
-	 * <p>
-	 * @param salary	the monthly salary
-	 */
-	public void setMonthlySalary(double salary)
-	{
-		Employee.salaryField/12 = salary;
-		out.println("Set Monthly Salary: " + salary);
-	}
+/**
+ * Getter for the monthly salary.
+ *
+ * @return	the value of the monthly salary
+ */
+public double getMonthlySalary()
+{
+	out.println("Get Monthly Salary: " + (person.salaryField)/12);
+	return person.salaryField/12;
+}
 
-	/**
-	 * String identifying and describing this employee.
-	 *
-	 * @return the string
-	 */
-	@Override
-	public String toString()
-	{
-		return super.toString() + " " + firstNameField + " " + lastNameField;
-	}
+/**
+ * Setter for the monthly salary.
+ * <p>
+ * @param salary	the monthly salary
+ */
+public void setMonthlySalary(double salary)
+{
+	salary = getMonthlySalary();
+	out.println("Set Monthly Salary: " + salary);
+
+}
+
+/**
+ * String identifying and describing this employee.
+ *
+ * @return the string
+ */
+@Override
+public String toString()
+{
+	return super.toString() + " " + firstNameField + " " + lastNameField;
+	//return "toString not yet implemented";
+}
 }
